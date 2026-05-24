e14994469ae5c1690e1e8f355b22795f9e14670b
peacebringer1 <187161126+peacebringer1@users.noreply.github.com>
2026-05-24 11:07:00 +0500

    prepare=dependencies.sh: functionality of package installation

--- prepare-dependencies.sh
+++ prepare-dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DISTRIBUTION=$(cat /etc/os-release | grep "^ID")
+DIRECTORY="/work"
 
 detect_distribution() {
     if [ $DISTRIBUTION = "ID=debian" ]; then
@@ -12,5 +13,32 @@ detect_distribution() {
     fi
 }
 
+__arch_install_packages(){
+    PACKAGE_FILE="$DIRECTORY/arch-dependencies.txt"
+    package_list=$(cat $PACKAGE_FILE)
+    $(pacman -Syu --noconfirm $package_list)
+}
+
+__debian_install_packages(){
+    PACKAGE_FILE="$DIRECTORY/debian-dependencies.txt"
+    package_list=$(cat $PACKAGE_FILE)
+    $(apt update -y && apt upgrade -y)
+    # echo "PACKAGES: $package_list"
+    $(apt install $package_list)
+}
+
+install_packages(){
+    if [ $DISTRIBUTION = "debian" ]; then
+        # error 127 if unknown package
+        __debian_install_packages
+    elif [ $DISTRIBUTION = "arch" ]; then
+        # error 127 if unknown package
+        __arch_install_packages
+    else
+        echo "Cannot install dependencies, because distribution is unsupported"
+    fi
+}
+
 detect_distribution
+install_packages
 
